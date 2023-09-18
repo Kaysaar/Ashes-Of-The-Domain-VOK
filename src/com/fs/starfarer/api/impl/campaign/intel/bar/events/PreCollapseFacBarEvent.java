43a3a8eafff6fd80195c6f595f6b1285560745c9
Wiktor Heblewski <240681@edu.p.lodz.pl>
2023-09-18 22:24:50 +0200

    Update Version Checker

--- src/com/fs/starfarer/api/impl/campaign/intel/bar/events/PreCollapseFacBarEvent.java
+++ src/com/fs/starfarer/api/impl/campaign/intel/bar/events/PreCollapseFacBarEvent.java
@@ -13,6 +13,7 @@ import com.fs.starfarer.api.util.Misc;
 import com.fs.starfarer.combat.entities.terrain.Planet;
 import data.plugins.AoDCoreModPlugin;
 
+import java.awt.*;
 import java.util.*;
 
 public class PreCollapseFacBarEvent extends BaseBarEvent {
@@ -65,13 +66,15 @@ public class PreCollapseFacBarEvent extends BaseBarEvent {
 
     }
 
-    //TODO Rewrite here dialog as this is from historaion
     protected String getPrompt() {
-        return "An old veteran sits alone at a table, scrolling thorugh, what seems to be a very old Tripad, which does not match anything, that has been seen in Sector";
+        return "A veteran spacer with some ornamental pre-Collapse relics scattered like ancient wards all over their clothes " + person.getHeOrShe() +
+                "casually scrolls through an odd looking TriPad that looks more dated than anyone in this bar. Sturdy enough to outlive everyone in the bar as well.";
     }
 
+
     protected String getOptionText() {
-        return "Go over to " + person.getManOrWoman() + " with old Tripad and ask what " + person.getHeOrShe() + " wants";
+        return "Sit next to scavenger with an old TriPad and listen to "
+                + person.getHisOrHer() + " stories about pre-Collapse relics.";
     }
 
     @Override
@@ -111,32 +114,38 @@ public class PreCollapseFacBarEvent extends BaseBarEvent {
         options.clearOptions();
 
         if (option == OptionId.GREETING) {
-            text.addPara("Small Introduction");
-            options.addOption("Option 1 : Listen", OptionId.GREETNG_CONTINUE_1);
-            options.addOption("Option 2 : Ignore", OptionId.END_CONVERSATION_START);
+            text.addPara("Anyone who spends enough time in the endless ruins of the Sector periphery would look and sound odd, and this scavenger is no exception." +
+                    "You're sure that most of the weird relics "
+                    + person.getHeOrShe() + " demonstrated to you are just IFF identification equipment to avoid trouble from automated systems,"+
+                    "yet there is something arcane in how they operate and talk about it. After a few minutes of showing you rather useless but rare items,  what you presume was a way for scavenger to establish" + person.getHisOrHer()
+                    + " credibility, they offer to tell you about recently failed expedition and, what’s more importantly, its target.");
+            options.addOption("Ask  "+ person.getHimOrHer()+" about the lost expedition that was looking for Domain tech.", OptionId.GREETNG_CONTINUE_1);
+            options.addOption("Thank  "+ person.getHimOrHer()+" for insights on Domain tech and politely end conversation.", OptionId.END_CONVERSATION_START);
             return;
         }
         if (option == OptionId.GREETNG_CONTINUE_1) {
-            text.addPara("Story about failed expedition");
-            options.addOption("Your attention? What do you mean? ", OptionId.WHAT_DO_YOU_HAVE);
+            text.addPara("\"Robust and experienced people, all veterans.\" " + person.getHeOrShe()
+                    + " said with a much more serious expression now. \"Wasn't just some dumb meat from the Core. " +
+                    "Fleet went dark after embarking from the last resupply point and we received no signal from them for a few cycles now. "+
+                    "The group that sent them decided not to bother with more expeditions.\"");
+            options.addOption("Ask "+ person.getHimOrHer()+"about the expedition goal.",OptionId.WHAT_DO_YOU_HAVE);
+
             return;
         }
         if (option == OptionId.WHAT_DO_YOU_HAVE) {
-            text.addPara("Moment where Pre Collapse Fac is mentioned");
-            options.addOption("Pre Collapse fac ? ", OptionId.MORE_INFO);
-            return;
-        }
-        if (option == OptionId.MORE_INFO) {
-            //Show planet
-            text.addPara("More Info about what could be there");
-            options.addOption("Where i can find it ? ", OptionId.PRICE);
+            text.addPara("\"And as for the expedition target…\" Scavenger just showed you a few orbital scan pictures on" + person.getHisOrHer() +
+                    " TriPad. Several Domain structures, in pristine condition. No, not just structures. It's a research facility. Scavenger smiles as "
+                    + person.getHeOrShe() + "sees you recognizing the real value of this information. It won’t be cheap for you, that’s for sure.");
+            options.addOption("Tell the scavenger that you need to hear a price before making any decisions.", OptionId.PRICE);
+
             return;
         }
         if (option == OptionId.PRICE) {
+            String money = Misc.getDGSCredits(100000);
+            text.addPara("With a few taps on TriPad " + person.getHeOrShe() + "just pull up an already filled out contract for %s credits. You just need to confirm it with your own.", Color.ORANGE,""+money);
+            options.addOption("Agree to pay the price and get coordinates for the Domain research facility.", OptionId.END_CONVERSATION);
+            options.addOption("Decide not to follow footsteps of lost expedition, and decline the offer.", OptionId.END_CONVERSATION_START);
 
-            text.addPara("Price : Money 100k ");
-            options.addOption("Yes", OptionId.END_CONVERSATION);
-            options.addOption("No ", OptionId.END_CONVERSATION_START);
             if (Global.getSector().getPlayerFleet().getCargo().getCredits().get() < 100000) {
                 options.setEnabled(OptionId.END_CONVERSATION, false);
             }
@@ -146,7 +155,11 @@ public class PreCollapseFacBarEvent extends BaseBarEvent {
             return;
         }
         if (option == OptionId.END_CONVERSATION) {
-            text.addPara("Telling location and showing location");
+            String money = Misc.getDGSCredits(100000);
+            text.addPara("Lost "+money,Misc.getNegativeHighlightColor());
+            text.addPara("\"Excellent. And good luck, starfarer.\" Your TriPad pings and you see the navigation information on "+targetPlanet.getName()+" in "+targetPlanet.getName()+" system. With that, "
+                    + person.getHeOrShe() + " gets up and walks away from a bar without finishing" + person.getHisOrHer() + " drink. "
+                    + "In a few seconds, you are left alone with an image of the planet on your TriPad.");
             Global.getSector().getPlayerFleet().getCargo().getCredits().subtract(100000);
             String icon = Global.getSettings().getSpriteName("intel", "red_planet");
             Set<String> tags = new LinkedHashSet<String>();
@@ -156,7 +169,7 @@ public class PreCollapseFacBarEvent extends BaseBarEvent {
                     "Destination: " + targetPlanet.getStarSystem().getName(), Misc.getBasePlayerColor(),
                     true, icon, null, tags);
 
-            options.addOption("Goooood", OptionId.LEAVE);
+            options.addOption("Finish your drink and leave with the information on the ancient laboratory that already got one scavenger fleet killed.", OptionId.LEAVE);
             ArrayList<PlanetAPI> planets = (ArrayList<PlanetAPI>) Global.getSector().getPersistentData().get(AoDCoreModPlugin.preCollapseFacList);
             planets.remove(targetPlanet);
             addIntel();
@@ -165,8 +178,9 @@ public class PreCollapseFacBarEvent extends BaseBarEvent {
 
         }
         if (option == OptionId.END_CONVERSATION_START) {
-            text.addPara("Text about maybe next time starfarer, a bit of dissapointment");
-            options.addOption("Essa", OptionId.LEAVE);
+            text.addPara("\"The choice is yours. Should you change your mind, I will be here, but not for long. "+
+                    "A lot of people would like to buy this kind of information, with creds or blood. They just don't know this, yet.\"");
+            options.addOption("Take your leave.", OptionId.LEAVE);
             return;
         }
         if (option == OptionId.LEAVE) {
