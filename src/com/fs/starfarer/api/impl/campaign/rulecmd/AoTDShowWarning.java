0191ac5c0f879c1fbc37bd19ffa05c45a5b231de
Wiktor Heblewski <240681@edu.p.lodz.pl>
2024-07-19 11:03:43 +0200

    2.2.0 Patch : add warning when colonizing hyperhshunt

---
+++ src/com/fs/starfarer/api/impl/campaign/rulecmd/AoTDShowWarning.java
@@ -0,0 +1,16 @@
+package com.fs.starfarer.api.impl.campaign.rulecmd;
+
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.util.Misc;
+
+import java.util.List;
+import java.util.Map;
+
+public class AoTDShowWarning extends BaseCommandPlugin{
+    @Override
+    public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
+        dialog.getTextPanel().addPara("Warning, shields are unstable, and they should be repaired as soon as possible",Misc.getNegativeHighlightColor());
+        return true;
+    }
+}
