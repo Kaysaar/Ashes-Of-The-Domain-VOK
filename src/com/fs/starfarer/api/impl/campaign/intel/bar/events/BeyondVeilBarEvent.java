38fe2dbb2044b1bc56b2938d48d8f9b626dc8baa
Purple Nebula <NebulaModding@hotmail.com>
2025-11-06 14:47:42 +0100

    BeyondVeilBarEvent.java Fixes

--- src/com/fs/starfarer/api/impl/campaign/intel/bar/events/BeyondVeilBarEvent.java
+++ src/com/fs/starfarer/api/impl/campaign/intel/bar/events/BeyondVeilBarEvent.java
@@ -84,13 +84,13 @@ public class BeyondVeilBarEvent extends BaseBarEventWithPerson {
         regen(dialog.getInteractionTarget().getMarket());
 
         TextPanelAPI text = dialog.getTextPanel();
-        text.addPara("A rough-looking veteran spacer with strange cybernetic augmentations seems to have been staring at you for while now " +
-                "it's a quite unnerving...");
+        text.addPara("A rough-looking veteran spacer with strange cybernetic augmentations seems to have been staring at you for a while now, " +
+                "making you feel quite unnerving...");
 
 //		Color c = Misc.getHighlightColor();
 //		c = Misc.getHighlightedOptionColor();
 
-        dialog.getOptionPanel().addOption("Ask the stranger why are they staring at you. ", this,
+        dialog.getOptionPanel().addOption("Ask the stranger why they are staring at you.", this,
                  Color.ORANGE,null);
     }
 
@@ -126,32 +126,32 @@ public class BeyondVeilBarEvent extends BaseBarEventWithPerson {
         switch (option) {
             case INIT:
                 text.addPara("\"Oh, it's nothing. Let's just say you caught my attention.\"");
-                options.addOption("Your attention? What do you mean by that? ", OptionId.INTRUIGED);
-                options.addOption("Suggest that " + getHeOrShe() + " mistook you for someone else.", OptionId.LEAVE);
+                options.addOption(getHisOrHer() + " attention? What does " + getHeOrShe().toLowerCase() + " mean by that?", OptionId.INTRUIGED);
+                options.addOption("Suggest that " + getHeOrShe().toLowerCase() + " mistook you for someone else.", OptionId.LEAVE);
                 break;
             case INTRUIGED:
-                text.addPara("\"Look, space might be a big place, but there is no way you can't hear about " + Global.getSector().getPlayerPerson().getName().getFullName() +
-                        "\"\n\" Believe it or not, some people even began calling you the \"The Seeker of Knowledge\". Your deeds haven't gone unnoticed.\"");
+                text.addPara("\"Look, space might be a big place, but there is no way you can't hear about " + Global.getSector().getPlayerPerson().getName().getFullName() + ".\"" +
+                        "\n\"Believe it or not, some people even began calling you the 'The Seeker of Knowledge'. Your deeds haven't gone unnoticed.\"");
 
-                options.addOption("What does that mean for me?", OptionId.CONTINUE_1);
+                options.addOption("\"What does that mean for me?\"", OptionId.CONTINUE_1);
                 break;
             case CONTINUE_1:
                 text.addPara("\"Plenty of people out there are keeping tabs on you and what you've been doing as of late. Some look for signs of danger, threats, that sort of thing, and if there's nothing to be concerned about they just move on; others, " +
-                        "like me, look deeper and think that you just might be the right person for the kind of valuable intel I have in my possession.\"");
+                        "like me, look deeper and think that you might just be the right person for the kind of valuable intel I have in my possession.\"");
                 text.addPara(getHeOrShe() + " leans back, giving the insignia on their uniform a quick glance before continuing.");
 
                 options.addOption("Continue", OptionId.CONTINUE_2);
                 break;
             case CONTINUE_2:
-                text.addPara("Back in the olden days, I was a part of an Hegemony Expeditionary Fleet. We had only one task, to investigate some odd signals coming from a planet far away from the Core Worlds. A simple job. At least on paper, that is." +
-                        "It was supposed to be a simple, quick investigation, but when we got there it turned into complete, utter slaughter. Entire fleet of strange ships, the likes of which I have never seen before, appeared out of thin air and annihilated the entire Expedition fleet. I'm the sole survivor of this massacre. " +
-                        "To this day I wonder if Command actually knew what kind of danger they were sending us into or they didn't and that's the just unfortunate result. Don't know which one is worse, to be honest.");
+                text.addPara("\"Back in the olden days, I was a part of a Hegemony Expeditionary Fleet. We had only one task, to investigate some odd signals coming from a planet far away from the Core Worlds. A simple job. At least on paper, that is. " +
+                        "It was supposed to be a simple, quick investigation, but when we got there it turned into a complete and utter slaughter. An entire fleet of strange ships, the likes of which I have never seen before, appeared out of thin air and annihilated the entire Expedition fleet. I'm the sole survivor of this massacre. " +
+                        "To this day I wonder if Command actually knew what kind of danger they were sending us into or if they didn't and that was just the unfortunate result. Don't know which one is worse, to be honest.\"");
 
 
                 options.addOption("\"Where was this planet located?\"", OptionId.WHERE_WAS_SYSTEM);
                 break;
             case WHERE_WAS_SYSTEM:
-                text.addPara(getHeOrShe() + " slides the tri-pad across the table, a blinking set of coordinates pointing in a direction well outside the Core Worlds. \nAll the data I have is right there. This is the system where we were ambushed.");
+                text.addPara(getHeOrShe() + " slides the tri-pad across the table, a blinking set of coordinates pointing in a direction well outside the Core Worlds. \n\"All the data I have is right there. This is the system where we were ambushed.\"");
                 String icon = Global.getSettings().getSpriteName("intel", "red_planet");
                 Set<String> tags = new LinkedHashSet<String>();
                 tags.add(Tags.INTEL_MISSIONS);
