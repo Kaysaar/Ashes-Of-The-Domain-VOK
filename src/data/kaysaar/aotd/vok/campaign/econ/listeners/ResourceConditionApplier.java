c0135b87bd4bfafb840ccf834d5c944c24e3db47
Wiktor Heblewski <240681@edu.p.lodz.pl>
2026-02-23 06:42:51 +0100

    4.2.1

--- src/data/kaysaar/aotd/vok/campaign/econ/listeners/ResourceConditionApplier.java
+++ src/data/kaysaar/aotd/vok/campaign/econ/listeners/ResourceConditionApplier.java
@@ -13,6 +13,7 @@ public class ResourceConditionApplier implements PlayerColonizationListener, Eco
     public static void applyResourceConditionToAllMarkets() {
         for (MarketAPI m : Global.getSector().getEconomy().getMarketsCopy()) {
             WaterMiningCond.applyIndustryUpgradeCondition(m);
+            AoDFoodDemand.applyResourceCond(m);
         }
     }
 
@@ -20,7 +21,7 @@ public class ResourceConditionApplier implements PlayerColonizationListener, Eco
     public void reportPlayerColonizedPlanet(PlanetAPI planetAPI) {
         MarketAPI m = planetAPI.getMarket();
         WaterMiningCond.applyIndustryUpgradeCondition(m);
-
+        AoDFoodDemand.applyResourceCond(m);
     }
 
     @Override
